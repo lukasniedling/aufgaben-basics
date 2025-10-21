ae6ceab82097c4d403094e1ae9a57d37e82b1574
LukasNiedling <lukasniedling2006@gmail.com>
2025-10-21 15:15:38 +0200

    Verushc AUfgabe

--- calculations/numbers/count_divisors.go
+++ calculations/numbers/count_divisors.go
@@ -2,6 +2,16 @@ package numbers
 
 // Erwartet eine Zahl n >= 1 und liefert die Anzahl der Teiler dieser Zahl zurück.
 func CountDivisors(n int) int {
-	// TODO
-	return 0
+	var x int
+	if n < 1 {
+
+		for i := 1; i < n; i++ {
+			if n%i == 0 {
+				x++
+			}
+		}
+
+	}
+	return x
+
 }
